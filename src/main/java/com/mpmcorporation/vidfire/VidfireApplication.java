84db59b4d7268409c0ef81ff4b2903c6d9ff9507
PauloC-Souza <paulocesar.dg09@gmail.com>
2023-06-27 20:33:55 -0300

    Project init

---
+++ src/main/java/com/mpmcorporation/vidfire/VidfireApplication.java
@@ -0,0 +1,13 @@
+package com.mpmcorporation.vidfire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VidfireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VidfireApplication.class, args);
+	}
+
+}
